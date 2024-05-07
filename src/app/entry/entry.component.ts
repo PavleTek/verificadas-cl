007ae7b8e11ba48c73d53780872b01b5ad9489ed
Pavle <pavle@pavletek.com>
2024-05-07 16:49:49 -0400

    meta and title for age verification page

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-entry',
@@ -11,9 +12,16 @@ import { ButtonModule } from 'primeng/button';
 })
 export class EntryComponent {
   userPassword: string = '';
-  constructor(private router: Router) {}
+  constructor(private router: Router, private titleService: Title, private metaService: Meta) {}
   confirmEntry(): void {
     localStorage.setItem('ageVerified', 'true');
     this.router.navigate(['/escorts']);
+  }
+  ngOnInit() {
+    this.titleService.setTitle('Confirmación de Edad - Verificadas.cl');
+    this.metaService.updateTag({
+      name: 'description',
+      content: 'Verificación de edad requerida para ingresar a Verificadas.cl Confirme mayoría de edad para acceder de forma segura y discreta a nuestra plataforma',
+    });
   }
 }
