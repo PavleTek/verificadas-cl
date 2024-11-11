403c93303dd9b25b9c875336e73900be17b17e79
Pavle <pavle@pavletek.com>
2024-11-11 12:41:02 -0300

    environent changed for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,19 @@
-// export const environment = {
-//   production: true,
-//   noDisplayValue: 'No Especificar',
-//   baseMultimediaUrl: 'https://cdn.verificadas.cl',
-//   baseUrl: 'https://api.verificadas.cl',
-//   baseImageUrl: 'https://cdn.verificadas.cl/images',
-//   basePendingimageUrl: 'https://cdn.verificadas.cl/pending-images',
-//   baseAccessUrl: 'https://verificadas.cl',
-// };
-
 export const environment = {
-  production: false,
+  production: true,
   noDisplayValue: 'No Especificar',
-  baseUrl: 'http://localhost:3000',
-  baseMultimediaUrl: 'http://localhost:3100',
-  baseImageUrl: 'http://localhost:3100/images',
-  basePendingimageUrl: 'http://localhost:3100/pending-images',
-  baseAccessUrl: 'http://localhost:4200',
-
+  baseMultimediaUrl: 'https://cdn.verificadas.cl',
+  baseUrl: 'https://api.verificadas.cl',
+  baseImageUrl: 'https://cdn.verificadas.cl/images',
+  basePendingimageUrl: 'https://cdn.verificadas.cl/pending-images',
+  baseAccessUrl: 'https://verificadas.cl',
 };
+
+// export const environment = {
+//   production: false,
+//   noDisplayValue: 'No Especificar',
+//   baseUrl: 'http://localhost:3000',
+//   baseMultimediaUrl: 'http://localhost:3100',
+//   baseImageUrl: 'http://localhost:3100/images',
+//   basePendingimageUrl: 'http://localhost:3100/pending-images',
+//   baseAccessUrl: 'http://localhost:4200',
+// };
